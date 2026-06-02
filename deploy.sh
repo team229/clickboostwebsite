588ef76ac98296a03cff933a93df9969b8474744
Abhay Sharma <whoabhaysharma@gmail.com>
2026-06-02 21:26:05 +0530

    Add GitHub Actions S3 deployment workflow and update copyright year

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,19 @@ if [ -z "$BUCKET" ]; then
   exit 1
 fi
 
+ENDPOINT="${S3_ENDPOINT_URL:-}"
+AWS_ARGS=()
+if [ -n "$ENDPOINT" ]; then
+  AWS_ARGS+=(--endpoint-url "$ENDPOINT")
+fi
+
 echo "Building Tailwind CSS..."
 npx tailwindcss -i ./input.css -o ./styles.css --minify
 
 echo "Deploying to s3://$BUCKET ..."
 
 aws s3 sync . "s3://$BUCKET" \
+  "${AWS_ARGS[@]}" \
   --exclude ".git/*" \
   --exclude ".gitignore" \
   --exclude "node_modules/*" \
@@ -22,11 +29,12 @@ aws s3 sync . "s3://$BUCKET" \
   --exclude "tailwind.config.js" \
   --exclude "input.css" \
   --exclude "deploy.sh" \
-  --exclude ".gitattributes" \
+  --exclude ".github/*" \
   --delete
 
 echo "Setting cache headers..."
 aws s3 cp "s3://$BUCKET" "s3://$BUCKET" \
+  "${AWS_ARGS[@]}" \
   --exclude "*" \
   --include "*.html" \
   --metadata-directive REPLACE \
@@ -34,10 +42,11 @@ aws s3 cp "s3://$BUCKET" "s3://$BUCKET" \
   --recursive
 
 aws s3 cp "s3://$BUCKET" "s3://$BUCKET" \
+  "${AWS_ARGS[@]}" \
   --exclude "*" \
   --include "styles.css" \
   --metadata-directive REPLACE \
   --cache-control "public, max-age=31536000, immutable" \
   --recursive
 
-echo "Done! Site deployed to https://$BUCKET.s3.amazonaws.com"
+echo "Done!"
